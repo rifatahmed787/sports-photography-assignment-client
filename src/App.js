19829cc0a83aacc1158a2dc6c3634ba48b05bb02
Rifat <mdrifatahmed787@gmail.com>
2022-11-08 01:55:23 +0600

    header section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import { ToastContainer } from "react-toastify";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/Routes/Router";
 
 function App() {
   return (
     <div className="">
+      <RouterProvider router={router}></RouterProvider>
       <ToastContainer />
     </div>
   );
--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,7 +1,73 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  // const { user, logOut } = useContext(AuthContext);
+
+  // const handleLogOut = () => {
+  //   logOut()
+  //     .then(() => {})
+  //     .catch((error) => console.error(error));
+  // };
+  // const menuItems = (
+  //     <>
+  //       <li className="font-semibold mr-2">
+  //         <Link to="/">Home</Link>
+  //       </li>
+  //       {user?.email ? (
+  //         <>
+  //           <li className="font-semibold mr-2">
+  //             <Link to="/orders">Orders</Link>
+  //           </li>
+  //           <li className="font-semibold mr-2">
+  //             {/* <button onClick={handleLogOut}>Log out</button> */}
+  //           </li>
+  //         </>
+  //       ) : (
+  //         <>
+  //           <li className="font-semibold mr-2">
+  //             <Link to="/login">Log in</Link>
+  //           </li>
+  //           <li className="font-semibold">
+  //             <Link to="/signup">Sign up</Link>
+  //           </li>
+  //         </>
+  //       )}
+  //     </>
+  //   );
+
+  const menuItems = (
+    <>
+      <li className="font-semibold mr-2">
+        <Link to="/">Home</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-gray-400">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden"></label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuItems}
+          </ul>
+        </div>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          {/* <img src={logo} alt="" /> */}
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-outline btn-success">Appoinment</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
