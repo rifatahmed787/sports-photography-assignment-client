68e89a7bffb44c20a217f2fb62c3ec56a8a5fb4e
Rifat <mdrifatahmed787@gmail.com>
2022-11-08 23:45:05 +0600

    authprovider added

--- src/components/contexts/AuthProvider.js
+++ src/components/contexts/AuthProvider.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
-const AuthProvider = () => {
-  return <div></div>;
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
